51ea04320908cde21a7058df866fafb5179c208d
Mazen Tarek <104716013+mazen-tech@users.noreply.github.com>
2024-04-29 18:52:24 +0200

    Update swap.c

--- Operations/swap.c
+++ Operations/swap.c
@@ -7,5 +7,48 @@ static int swap(t_push_swap **stack)
     int tmp_val
     int tmp_index;
 
-     
+    if(ft_stacksize(*stack) < 2)
+        return (-1);
+
+    head = *stack;
+    next = *stack->next;
+
+    if(!head && !next)
+        return (error_message("Error!"));
+    tmp_val = head->value;
+    tmp_index = head->index;
+    head->value = next->value;
+    head->index = next->index;
+    next->value = tmp->value;
+    next->index = tmp->index;
+    return (0);
+}
+
+//to swap the first two node in the stack a 
+int sa(t_push_swap **stack_a)
+{
+    if(swap(stack_a == -1))
+        return (-1);
+    write(1, "sa\n", 3);
+    return (0);   
+}
+
+//to swap the firt two nodes in the srack b
+int sb(t_push_swap **stack_b)
+{
+    if(swap(stack_b == -1))
+        return (-1);
+    write(1, "sb\n", 3);
+    return (0);
+}
+
+//to swap the first two nodes in stack a and b at the same time
+int ss(t_push_swap **stack_a, t_push_swap **stack_b)
+{
+    if((ft_stacksize(*stack_a) < 2) || (ft_stacksize(*stack_b) < 2))
+        return (-1);
+    swap(stack_a);
+    swap(stack_b);
+    write(1, "ss\n", 3);
+    return (0);
 }
